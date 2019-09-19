ab3522ae73a8b6951ad09dd3381caa40747db8cf
pierreberchtold <pierre.berchtold@gmail.com>
2019-09-19 14:46:48 +0200

    Add Main Menu to Header

--- sodadivi/template-parts/header/site-branding.php
+++ sodadivi/template-parts/header/site-branding.php
@@ -12,4 +12,18 @@
 		<div class="site-logo"><?php the_custom_logo(); ?></div>
 	<?php endif; ?>
 
+	<?php if ( has_nav_menu( 'main-menu' ) ) : ?>
+		<nav id="site-navigation" class="main-navigation" aria-label="<?php esc_attr_e( 'Top Menu', 'sodadividomain' ); ?>">
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'main-menu',
+					'menu_class'     => 'main-menu',
+					'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+				)
+			);
+			?>
+		</nav><!-- #site-navigation -->
+	<?php endif; ?>
+
 </div><!-- .site-branding -->
